851dcd52da05b9b8dd8e22884c8ae9d4de719a39
Gopal Shah <simplecooldude1@gmail.com>
2017-01-16 16:21:28 -0800

    add a second handler

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var Voke = require("../dist/voke.min.js");
 var mocha = require("mocha");
 var expect = require("chai").expect;
 
-var emitter, tmp;
+var emitter, tmp, tmp2;
 
 describe('Voke', function() {
   describe('instance', function() {
@@ -19,6 +19,13 @@ describe('Voke', function() {
         tmp = "Called";
       });
       expect(emitter.events['evt'][0]).to.be.a('function');
+    });
+
+    it('should create a handler that takes an event', function() {
+      emitter.on('evt', function(e) {
+        tmp2 = e;
+      });
+      expect(emitter.events['evt'][1]).to.be.a('function');
     });
   });
 
