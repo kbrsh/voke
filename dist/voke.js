b16829553a2da43dfb9dc25438fe438e341ed992
Gopal Shah <simplecooldude1@gmail.com>
2017-01-16 16:07:19 -0800

    first build

---
+++ dist/voke.js
@@ -0,0 +1,44 @@
+/*
+* Voke 1.0.0
+* Copyright 2017, Kabir Shah
+* https://github.com/KingPixil/voke/
+* Free to use under the MIT license.
+* https://kingpixil.github.io/license
+*/
+(function(root, factory) {
+  (typeof module === "object" && module.exports) ? module.exports = factory() : root.Voke = factory();
+}(this, function() {
+
+		var error = function(msg) {
+    	console.error('[Wake] ERR: ' + msg);
+    }
+
+    function Voke() {
+    	this.events = {};
+    }
+
+
+    Voke.prototype.on = function(event, action) {
+    	if(this.events[event]) {
+      	this.events[event].push(action);
+      } else {
+      	this.events[event] = [action];
+      }
+    }
+
+    Voke.prototype.emit = function(event, meta) {
+    	if(!this.events[event]) {
+      	error("Event '" + event + "' Not Found");
+        return;
+      }
+
+      for(var i = 0; i < this.events[event].length; i++) {
+      	var evtObj = meta || {};
+        evtObj.type = event;
+      	this.events[event][i](evtObj);
+      }
+
+    }
+
+    return Voke;
+}));
