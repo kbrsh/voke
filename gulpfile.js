7443ed62ed3841a1a1b4b06ca10ea3dd9dda6dd9
Gopal Shah <simplecooldude1@gmail.com>
2017-01-16 16:03:06 -0800

    basic gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var gulp = require('gulp');
+var pkg = require('./package.json');
+var comment = '\/*\r\n* Voke ' + pkg.version + '\r\n* Copyright 2017, Kabir Shah\r\n* https:\/\/github.com\/KingPixil\/voke\/\r\n* Free to use under the MIT license.\r\n* https:\/\/kingpixil.github.io\/license\r\n*\/\r\n';
+
+// Build Voke
+gulp.task('build', function () {
+  return gulp.src(['./index.js'])
+    .pipe($.header(comment))
+    .pipe($.size())
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('minify', ['build'], function() {
+  return gulp.src(['./dist/voke.js'])
+    .pipe(uglify())
+    .pipe($.header(comment))
+    .pipe($.size())
+    .pipe($.concat('voke.min.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+// Run Tests
+gulp.task('test', function () {
+
+});
+
+
+// Default task
+gulp.task('default', ['build', 'minify']);
