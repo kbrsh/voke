143c2c410da88e69771da40bbe9091387b2ed417
Gopal Shah <simplecooldude1@gmail.com>
2017-01-16 16:18:38 -0800

    dd some tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,25 @@
 var Voke = require("../dist/voke.min.js");
 
 var mocha = require("mocha");
-var should = require("chai").should();
+var expect = require("chai").expect;
 
-describe('', function() {
+var emitter;
 
+describe('Voke', function() {
+  describe('instance', function() {
+    it('should create an instance', function() {
+      emitter = new Voke();
+      expect(emitter).to.not.be.null;
+    });
+  });
+
+  describe('#on', function() {
+    var tmp;
+    it('should create a handler', function() {
+      emitter.on('evt', function() {
+        tmp = "Called";
+      });
+      expect(emitter.events['evt'][0]).to.be.a('function');
+    });
+  });
 });
