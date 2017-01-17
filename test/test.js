cc6cca3319422ce5ddf9b1e49545ebfc224a9f56
Gopal Shah <simplecooldude1@gmail.com>
2017-01-16 16:20:31 -0800

    should emit an event

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var Voke = require("../dist/voke.min.js");
 var mocha = require("mocha");
 var expect = require("chai").expect;
 
-var emitter;
+var emitter, tmp;
 
 describe('Voke', function() {
   describe('instance', function() {
@@ -14,12 +14,18 @@ describe('Voke', function() {
   });
 
   describe('#on()', function() {
-    var tmp;
     it('should create a handler', function() {
       emitter.on('evt', function() {
         tmp = "Called";
       });
       expect(emitter.events['evt'][0]).to.be.a('function');
+    });
+  });
+
+  describe('#emit()', function() {
+    it('should emit an event', function() {
+      emitter.emit('evt');
+      expect(tmp).to.equal('Called');
     });
   });
 });
